11f6bde86afe6b7874c73e44d1391ccebc70e262
Jack Franklin <jack@jackfranklin.net>
2012-11-13 16:00:31 +0000

    demo looking gooood

--- demo.js
+++ demo.js
@@ -2,13 +2,16 @@ $(function() {
   var tables = {};
   $("table").each(function(i, item) {
     var opts = {};
-    if(i == 2) { opts.applyOnInit = false; }
+    if(i == 2) { opts.stacked = true; opts.outOf = 60 }
     tables[item.id] = $.magnaCharta($(item), opts);
+
+    $(this).find('caption').append('<a href="" class="toggle">Toggle chart / table</a>');
   });
 
-  $("span").on("click", function() {
-    var table = $(this).nextAll("table").first();
+  $(".toggle").on("click", function(e) {
+    var table = $(this).parents("table");
     var tableMC = tables[table[0].id];
     (tableMC.$table.hasClass("mc-table") ? tableMC.revert() : tableMC.apply());
+    e.preventDefault();
   });
 });
