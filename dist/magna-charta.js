60c27b821f19b655e98c98896a9ceb30676e0fe1
Jack Franklin <jack@jackfranklin.net>
2012-11-14 14:56:59 +0000

    new grunt build with latest changes

--- dist/magna-charta.js
+++ dist/magna-charta.js
@@ -1,4 +1,4 @@
-/*! Magna Charta - v0.1.0 - 2012-11-13
+/*! Magna Charta - v0.1.0 - 2012-11-14
 * https://github.com/alphagov/magna-charta
  */
 
@@ -8,8 +8,7 @@
     this.init = function(table, options) {
       var defaults = {
         outOf: 65,
-        applyOnInit: true,
-        stacked: false
+        applyOnInit: true
       };
       this.options = $.extend({}, defaults, options);
       this.$table = table;
@@ -17,6 +16,9 @@
 
       // set the stacked option based on giving the table a class of mc-stacked
       this.options.stacked = this.$table.hasClass("mc-stacked");
+
+      // set the negative option based on giving the table a class of mc-negative
+      this.options.negative = this.$table.hasClass("mc-negative");
       this.$bodyRows = this.$table.find("tr");
 
       if(this.options.applyOnInit) {
@@ -32,16 +34,17 @@
     };
 
     this.revert = function() {
+      this.restoreText();
       this.removeWidths();
       this.removeClasses();
     };
 
     this.removeClasses = function() {
-      this.$table.removeClass("mc-table").find(".mc-key-cell, .mc-row, .mc-bar-cell").removeClass("mc-key-cell mc-row mc-bar-cell");
+      this.$table.removeClass("mc-table").find(".mc-key-cell, .mc-row, .mc-bar-cell, .mc-bar-negative, .mc-bar-positive").removeClass("mc-key-cell mc-row mc-bar-cell .mc-bar-negative .mc-bar-positive");
     };
 
     this.removeWidths = function() {
-      this.$table.find(".mc-bar-cell").css("width", "");
+      this.$table.find(".mc-bar-cell").css("width", "").css("margin-left", "");
     };
 
     this.utils = {
@@ -57,6 +60,9 @@
           if(values[i] > max) { max = values[i]; }
         }
         return max;
+      },
+      isNegative: function(value) {
+        return (value < 0);
       }
     };
 
@@ -66,28 +72,72 @@
     };
 
     this.calculateMaxWidth = function() {
+
+
+      // JS scoping sucks
       var that = this;
-      var resp = {
-        max: 0,
-        single: 0
-      };
+
+      // store the cell values in here so later
+      // so we can figure out the maximum value later
       var values = [];
+
+
+      // var to store the maximum negative value, (used only for negative charts)
+      var maxNegativeValue = 0;
+
+      // loop through every tr in the table
       this.$bodyRows.each(function(i, item) {
         var $this = $(item);
+
+        // the first td is going to be the key, so ignore it
         var $bodyCells = $this.find("td:not(:first)");
+
+        // if it's stacked, the last column is a totals, so we don't want that in our calculations
         if(that.options.stacked) {
-          // if it's stacked, the last column is a totals, so we don't want that in our calculations
           var $stackedTotal = $bodyCells.last().addClass("mc-stacked-total");
           $bodyCells = $bodyCells.filter(":not(:last)");
         }
+
+        // find all the header ccells and give them the right classes
         var $headCells = $this.find("th:not(:first, .total)").addClass("mc-key-cell");
+        // do the same with the first td in each row
         $this.find("td:first").addClass("mc-key-cell");
+
+        // store the total value of the bar cells in a row
+        // for anything but stacked, this is just the value of one <td>
         var cellsTotalValue = 0;
+
         $bodyCells.each(function(j, cell) {
+
           var $cell = $(cell).addClass("mc-bar-cell");
+
           var cellVal = that.utils.stripValue($cell.text());
+
           if(that.utils.isFloat(cellVal)) {
+
             var parsedVal = parseFloat(cellVal, 10);
+            var absParsedVal = Math.abs(parsedVal);
+
+            if(that.options.negative) {
+
+              if(that.utils.isNegative(parsedVal)) {
+
+                $cell.addClass("mc-bar-negative");
+
+                if(absParsedVal > maxNegativeValue) {
+                  maxNegativeValue = absParsedVal;
+                }
+
+              } else {
+
+                $cell.addClass("mc-bar-positive");
+              }
+
+            }
+            // now we are done with our negative calculations
+            // set parsedVal to absParsedVal
+            parsedVal = absParsedVal;
+
             if(!that.options.stacked) {
               cellsTotalValue = parsedVal;
               values.push(parsedVal);
@@ -97,23 +147,80 @@
           }
         });
 
+        // if stacked, we need to push the total value of the row to the values array
         if(that.options.stacked) { values.push(cellsTotalValue); }
-        resp.max = parseFloat(that.utils.returnMax(values), 10);
+
       });
+
+      var resp = {};
+
+      resp.max = parseFloat(that.utils.returnMax(values), 10);
       resp.single = parseFloat(this.options.outOf/resp.max, 10);
+
+      if(this.options.negative) {
+        resp.marginLeft = parseFloat(maxNegativeValue, 10) * resp.single;
+        resp.maxNegative = parseFloat(maxNegativeValue, 10);
+      }
+
+
       return resp;
     };
 
+    this.restoreText = function() {
+      $(".mc-bar-cell").each(function(i, item) {
+        var $cell = $(item);
+        var oldText = $cell.data("oldText");
+        if(oldText) {
+          $cell.text(oldText);
+        }
+      });
+    };
+
     this.applyWidths = function() {
+      this.restoreText();
       this.dimensions = this.calculateMaxWidth();
+
       var that = this;
+
       this.$bodyRows.each(function(i, row) {
+
         var $this = $(row);
+
         $this.find(".mc-bar-cell").each(function(j, cell) {
-          var val = parseFloat(that.utils.stripValue($(cell).text()), 10) * that.dimensions.single;
-          $(cell).css({
-            "width": val + "%"
-          });
+
+          var $cell = $(cell);
+
+          var parsedCellVal = parseFloat(that.utils.stripValue($cell.text()), 10);
+          var parsedVal = parsedCellVal * that.dimensions.single;
+
+          var absParsedCellVal = Math.abs(parsedCellVal);
+          var absParsedVal = Math.abs(parsedVal);
+
+          // apply the left margin to the positive bars
+          if(that.options.negative) {
+            if($cell.hasClass("mc-bar-positive")) {
+              $(cell).css("margin-left", that.dimensions.marginLeft + "%");
+            } else {
+              // if its negative but not the maximum negative
+              // we need to give it enough margin to push it further right to align
+              if(absParsedCellVal < that.dimensions.maxNegative ) {
+                // left margin needs to be
+                // (largestNegVal - thisNegVal)*single
+                var leftMarg = (that.dimensions.maxNegative - absParsedCellVal) * that.dimensions.single;
+                $cell.css("margin-left", leftMarg + "%");
+              }
+            }
+
+          }
+
+
+
+          $cell.css("width", absParsedVal + "%");
+
+          // set the text to be the absolute value
+          // but first save the old value
+          $cell.data("oldText", $cell.text());
+          $cell.text(absParsedCellVal);
         });
       });
     };
